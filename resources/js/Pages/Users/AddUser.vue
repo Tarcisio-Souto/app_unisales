2ef9872e47c6d1e91abad685d0d66aa26ca6f65d
tarcisio.souto <tss.labsi@gmail.com>
2022-05-10 21:09:50 -0300

    incluindo recursos do ViaCep no cadastro de usuários

--- resources/js/Pages/Users/AddUser.vue
+++ resources/js/Pages/Users/AddUser.vue
@@ -596,6 +596,78 @@ export default {
     races: Array,
     departments: Array
   },
+
+  mounted: () => {
+
+    $(document).ready(function() {
+
+            function limpa_formulário_cep() {
+                // Limpa valores do formulário de cep.
+                $("#inputStreet").val("");
+                $("#inputDistrict").val("");
+                $("#inputCity").val("");
+                $('select[name="inputState"]');
+                //$("#inputState").val("");
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#inputZipCode").blur(function() {
+
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
+
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
+
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
+
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#inputStreet").val("");
+                        $("#inputDistrict").val("");
+                        $("#inputCity").val("");
+                        $("#inputState").val("");
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+
+                                $("#inputStreet").val(dados.logradouro);
+                                $("#inputDistrict").val(dados.bairro);
+                                $("#inputCity").val(dados.localidade);
+                                //$("#inputState").val(dados.uf);
+                                $("#inputState option").filter(function() {
+                                  return this.text == dados.uf; 
+                                }).attr('selected', true);
+
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulário_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário_cep();
+                }
+            });
+        });
+
+  },
+
   data: () => {
     return {
       form: {
@@ -668,11 +740,14 @@ export default {
           this.form.dt_birth = null;
           this.form.genre = null;  
 
-
         },
         
       });
     },
   },
+
+  
+
+
 };
 </script>

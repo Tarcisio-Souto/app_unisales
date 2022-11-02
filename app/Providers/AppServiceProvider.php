370dcc4d9ce38970cad38ada0caa7358930a8fbf
tarcisio_souto <tss.labsi@gmail.com>
2022-11-02 12:17:10 -0300

    alterando o tamanho da string padrão para 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //if (config('app.env') === 'production') {
+        //    \url('https');
+        //}
+
+        Schema::defaultStringLength(191);
+
     }
 }

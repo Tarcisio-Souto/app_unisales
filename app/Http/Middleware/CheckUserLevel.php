acf6a2d646615afdaf6a93937e583040559e969f
tarcisio_souto <tss.labsi@gmail.com>
2022-11-22 22:36:57 -0300

    removendo middleware checkUserLevel do cadastro de usuário

---
+++ app/Http/Middleware/CheckUserLevel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class CheckUserLevel
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Recupera o e-mail do usuário logado
+        $cargo = auth()->user()->fk_position;
+
+        if ($cargo != 8) {
+            return Redirect::back()->withErrors(['accessLevel' => 'O acesso requer elevação.']);
+        }
+
+        return $next($request);
+    }
+}
